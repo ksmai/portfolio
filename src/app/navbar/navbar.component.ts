96fb86e86da6f3df260581c58115ea13da0acfb9
ksmai <kasingmai@gmail.com>
2017-05-05 00:06:06 +0800

    Add a sliding navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,16 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'port-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {
-  name = 'K.S. Mai';
+export class NavbarComponent implements OnInit {
+  show = false;
 
-  links = [
-    'About me',
-    'Projects',
-    'Contact',
-  ];
+  private lastScrollY = 0;
+  private wait = true;
+  private timer: any;
+
+  ngOnInit() {
+    setTimeout(() => this.wait = false, 15000);
+  }
+
+  scrollToAbout() {
+  }
+
+  scrollToProjects() {
+  }
+
+  scrollToContact() {
+  }
+
+  resetTimer() {
+    clearTimeout(this.timer);
+    this.timer = setTimeout(() => this.show = false, 2000);
+  }
+
+  @HostListener('window:scroll')
+  private onScroll() {
+    if (this.wait) {
+      return;
+    }
+
+    this.show = window.scrollY < this.lastScrollY;
+    this.lastScrollY = window.scrollY;
+    this.resetTimer();
+  }
 }
